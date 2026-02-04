0ed317f5684986c884b8fc75468afd26e1998f88
anvan5582-commits <anvan5582@gmail.com>
2026-02-04 17:17:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import os
-from flask import Flask, request, redirect
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.before_request
 def before_request():
-    # Koyeb передає заголовок X-Forwarded-Proto, щоб ми знали, який протокол у юзера
+    # Наш редірект на HTTPS
     if not request.is_secure and request.headers.get('X-Forwarded-Proto') == 'http':
         url = request.url.replace('http://', 'https://', 1)
         return redirect(url, code=301)
 
 @app.route("/")
 def home():
-    # Додаємо заголовок HSTS вручну для безпеки
-    response = app.make_response("<h1>Привіт! Тепер редірект працює на рівні коду! 🔐</h1>")
-    response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
-    return response
+    # Відкриваємо ваш HTML-файл з папки templates
+    return render_template("index.html") 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
